ee358eb86a76d9317d913ac2bdea39d61bf31e06
shuya <shuya@sfc.wide.ad.jp>
2017-09-04 16:32:00 +0900

    remove unnecessary js

--- js/script.js
+++ js/script.js
@@ -83,42 +83,6 @@ $(document).ready(function(e) {
 	});
 });
 
-/******************
-* = Gallery width *
-******************/
-$(function() {
-	var pause = 50; // will only process code within delay(function() { ... }) every 100ms.
-	$(window).resize(function() {
-		delay(function() {
-				var gallery_images = $('#slide-3 img');
-				
-				var images_per_row = 0;
-				if ( gallery_images.length % 2 == 0 ) {
-					images_per_row = gallery_images.length / 2;
-				} else {
-					images_per_row = gallery_images.length / 2 + 1;
-				}
-				
-				var gallery_width = $('#slide-3 img').width() * $('#slide-3 img').length;
-				gallery_width /= 2;
-				if ( $('#slide-3 img').length % 2 != 0 ) {
-					gallery_width += $('#slide-3 img').width();
-				}
-				
-				$('#slide-3 .row').css('width', gallery_width );
-				
-				var left_pos = $('#slide-3 .row').width() - $('body').width();
-				left_pos /= -2;
-				
-				$('#slide-3 .row').css('left', left_pos);
-			
-			},
-			pause
-		);
-	});
-	$(window).resize();
-});
-
 var delay = (function(){
 	var timer = 0;
 	return function(callback, ms){
@@ -237,42 +201,6 @@ jQuery(document).ready(function ($) {
 		},
 		function(e) {
 			$('.special-active-menu').remove();
-		}
-	);
-});
-
-/******************
-* = Gallery hover *
-******************/
-jQuery(document).ready(function ($) {
-	//Cache some variables
-	var images = $('#slide-3 a');
-	
-	images.hover(
-		function(e) {
-			var asta = $(this).find('img');
-			$('#slide-3 img').not( asta ).stop(false, false).animate(
-				{
-					opacity: .5
-				},
-				'fast',
-				'linear'
-			);
-			var zoom = $('<div class="zoom"></div>');
-			if ( $(this).hasClass('video') ) {
-				zoom.addClass('video');
-			}
-			$(this).prepend(zoom);
-		},
-		function(e) {
-			$('#slide-3 img').stop(false, false).animate(
-				{
-					opacity: 1
-				},
-				'fast',
-				'linear'
-			);
-			$('.zoom').remove();
 		}
 	);
 });
